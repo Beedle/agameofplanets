e036c869c26b375708b257f9c5dededfb9b94b4e
Sverre Blekastad Stensby <sverrebs@gmail.com>
2013-10-12 11:38:40 +0200

    Added Defence

---
+++ pvp/Assets/Scripts/Defence.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Defence : MonoBehaviour {
+	
+	public KeyCode mKey;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(mKey)) {
+			// Do defence stuffs
+			
+		}
+	}
+}
