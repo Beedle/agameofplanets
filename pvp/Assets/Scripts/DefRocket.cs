cfefb8b3529411ef0b38c9b87af89058fb8c13e2
Sverre Blekastad Stensby <sverrebs@gmail.com>
2013-10-12 11:48:45 +0200

    Added DefRocket.cs

---
+++ pvp/Assets/Scripts/DefRocket.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefRocket : Rocket {
+
+	// Use this for initialization
+	protected override void Start () {
+		mMass = 1f;
+	}
+	
+	
+}
