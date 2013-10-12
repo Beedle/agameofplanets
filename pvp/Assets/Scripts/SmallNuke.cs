49d3a7059cac9a77d1e8c0485c7c9750ee856489
Aleks <D3stny@gmail.com>
2013-10-12 16:09:36 +0200

    NUKES

---
+++ pvp/Assets/Scripts/SmallNuke.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmallNuke : Rocket {
+	
+	
+	
+	void Awake() {
+		mType = Rocket.Type.SMALL_NUKE;	
+	}
+	
+	public override float EnergyCost(){
+		return 10f;
+	}
+	
+}
